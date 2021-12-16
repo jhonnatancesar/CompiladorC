aec86df452b08cb06a0e2a2dd135b1eaba91caad
sodmlx2 <91387895+sodmlx2@users.noreply.github.com>
2021-12-16 11:35:03 -0300

    Update main.c

--- fontes/main.c
+++ fontes/main.c
@@ -7,7 +7,7 @@ int main(int argc,char *argv[]){
   FILE *codigo_fonte_saida;
 
   if(argc!=3){
-    printf("\nInterpretador/marco-v0.1\nUtilização: %s arquivo-entrada arquivo-saida\n\n",argv[0]);
+    printf("\nInterpretador/GRUPO-01-v0.1\nUtilização: %s arquivo-entrada arquivo-saida\n\n",argv[0]);
     exit(1);
   }
 
